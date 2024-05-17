c2ce4ed03fb22c2234383e65de213aceeaead53f
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-05-17 13:44:57 +0600

    footer done and dynamic year set up

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Shared/Header';
+import Footer from '../Shared/Footer';
 
 const Root = () => {
   return (
     <div>
       <Header></Header>
       <Outlet></Outlet>
-
+      <Footer></Footer>
     </div>
   );
 };
