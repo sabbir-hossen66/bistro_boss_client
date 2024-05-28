ac7403e55927558e6daa7c96c55f0b27b3cd5328
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-05-28 13:46:30 +0600

    jwt implementing

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,4 +1,5 @@
 import auth from "@/firebase/firebase.init";
+import useAxiosPublic from "@/hooks/useAxiosPublic";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 
@@ -8,6 +9,7 @@ const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null)
   const [loading, setLoading] = useState(true)
   const googleProvider = new GoogleAuthProvider();
+  const axiosPublic = useAxiosPublic();
 
   const createUser = (email, password) => {
     setLoading(true)
@@ -40,6 +42,19 @@ const AuthProvider = ({ children }) => {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, currentUser => {
       setUser(currentUser)
+      if (currentUser) {
+        const userInfo = { emal: currentUser.email }
+        axiosPublic.post('/jwt', userInfo)
+          .then(res => {
+            if (res.data.token) {
+              localStorage.setItem('access-token', res.data.token)
+            }
+          })
+      }
+      else {
+        // todo:remove token
+        localStorage.removeItem('access-token')
+      }
       setLoading(false)
     })
     return () => {
--- src/hooks/UseAxios.jsx
+++ src/hooks/UseAxios.jsx
@@ -4,6 +4,24 @@ const axiosSecure = axios.create({
   baseURL: 'http://localhost:5000'
 })
 const UseAxios = () => {
+  axiosSecure.interceptors.request.use(function (config) {
+    const token = localStorage.getItem('access-token')
+    console.log('request stopped by interceptors', token);
+    config.headers.authorization = `Bearer ${token}`;
+    return config;
+  },
+    function (error) {
+      return Promise.reject(error);
+    });
+
+  // intercepts 401 status 
+  axiosSecure.interceptors.response.use(function (response) {
+    return response;
+  }, (error) => {
+    const status = error.response.status;
+    console.log('status error', status);
+    return Promise.reject(error);
+  })
 
   return axiosSecure;
 };
